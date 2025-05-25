5bf0080b444428a74af1d1b649363e7471c6698d
はいいろ <mrashen.2gou@gmail.com>
2025-05-25 23:31:52 +0900

    :wrench: スクリプトを修正

--- .github/workflows/filter-and-update-submodule.ts
+++ .github/workflows/filter-and-update-submodule.ts
@@ -1,5 +1,5 @@
 `use strict`;
-import { exec } from "child_process";
+import { execSync } from "child_process";
 import PropertiesReader from "properties-reader";
 
 type GitModule = { path: string; url: string; branch?: string | null };
@@ -30,7 +30,10 @@ async function readGitModules(): Promise<GitModule[]> {
     });
 }
 
-const env: { getOrDefault: (key: string, defaultValue: string) => string, getOrThrow: (key: string) => string } = {
+const env: {
+    getOrDefault: (key: string, defaultValue: string) => string;
+    getOrThrow: (key: string) => string;
+} = {
     getOrDefault(key, defaultValue) {
         return process.env[key] ?? defaultValue;
     },
@@ -41,6 +44,21 @@ const env: { getOrDefault: (key: string, defaultValue: string) => string, getOrT
         return value;
     },
 };
+
+function runCode(_code: string | string[]): void {
+    let code: string[] = [];
+    if (typeof _code === "string") code.push(_code);
+    else code = _code;
+    code.forEach(line => {
+        try {
+            const output = execSync(line, { encoding: "utf-8" });
+            if (output !== "") console.log(`${output}`);
+        } catch (error: unknown) {
+            console.error(`Error executing command: ${line}`);
+            process.exit(1);
+        }
+    });
+}
 
 async function run() {
     const repoName = env.getOrThrow("REPOSITORY");
@@ -60,6 +78,21 @@ async function run() {
             `Repository ${repoName} found in .gitmodules, but branch is not correct.`
         );
         process.exit(0);
+    }
+    console.log(`Repository ${repoName} found in .gitmodules.`);
+
+    const pat = env.getOrThrow("ORG_PAT");
+    runCode([
+        `git config --global url."https://x-access-token:${pat}@github.com".insteadOf "https://github.com"`,
+        `git submodule update --remote --merge ${targetModule.path}`,
+    ]);
+    const sha = env.getOrDefault("SHA", "");
+    if (sha === "") {
+        runCode([
+            `echo "HEAD_SHA=$(git ls-remote https://x-access-token:${pat}@github.com/${repoName}.git refs/heads/${branch} | cut -f1)" >> $GITHUB_ENV`,
+        ]);
+    } else {
+        runCode(`echo "HEAD_SHA=${sha}" >> $GITHUB_ENV`);
     }
 }
 
